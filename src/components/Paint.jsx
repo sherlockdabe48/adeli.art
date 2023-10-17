1bd18b5ab3d7327ab8843197892951771e0cd0b1
Poramin Lueanskun <poramin_lueanskun@apple.com>
2023-10-17 13:35:08 +0800

    Create Paint

---
+++ src/components/Paint.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/components/Paint.css'
+
+
+const Paint = () => {
+  return (
+    <div className='container'>
+        <img className='image-container' src={require('../images/telescope.jpg')} alt=''/>
+    </div>
+  )
+}
+
+export default Paint
